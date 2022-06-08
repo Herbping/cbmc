76c06b7dd34ecaf891c14ae8f33c63e4845fbcb9
Qinheping Hu <qinhh@amazon.com>
2022-06-08 18:08:54 -0500

    convert function name of static function

--- src/goto-synthesizer/goto_synthesizer_verifier.cpp
+++ src/goto-synthesizer/goto_synthesizer_verifier.cpp
@@ -70,6 +70,24 @@ Author: Qinheping Hu
 #include <goto-instrument/loop_utils.h>
 #include <langapi/mode.h>
 
+irep_idt convert_static_function_name(const source_locationt &source_location)
+{
+  std::string path = source_location.get_file().c_str();
+  std::string base_filename = path.substr(path.find_last_of("/\\") + 1);
+
+  size_t dot = base_filename.find_last_of(".");
+  std::string name = base_filename.substr(0, dot);
+  std::string ext = base_filename.substr(dot + 1, base_filename.size() - dot);
+
+  // are there more ext for static file?
+  if(ext == "inl")
+  {
+    return ("__CPROVER_file_local_" + name + "_" + ext + "_") +
+           source_location.get_function().c_str();
+  }
+  return source_location.get_file();
+}
+
 goto_synthesizer_parse_optionst::loop_idt simple_verifiert::get_cause_loop_id(
   const goto_tracet &goto_trace,
   const goto_programt::const_targett violation)
@@ -90,12 +108,12 @@ goto_synthesizer_parse_optionst::loop_idt simple_verifiert::get_cause_loop_id(
       goto_programt::const_targett to;
 
       // get `from` a loop havoc instruction
+      irep_idt from_fun_name =
+        convert_static_function_name(step.pc->source_location());
       for(goto_programt::const_targett it =
-            parse_option.goto_model
-              .get_goto_function(step.pc->source_location().get_function())
+            parse_option.goto_model.get_goto_function(from_fun_name)
               .body.instructions.begin();
-          it != parse_option.goto_model
-                  .get_goto_function(step.pc->source_location().get_function())
+          it != parse_option.goto_model.get_goto_function(from_fun_name)
                   .body.instructions.end();
           ++it)
       {
@@ -106,14 +124,13 @@ goto_synthesizer_parse_optionst::loop_idt simple_verifiert::get_cause_loop_id(
       }
 
       // get `to` the instruction where violation happens
+      irep_idt to_fun_name =
+        convert_static_function_name(violation->source_location());
       for(goto_programt::const_targett it =
-            parse_option.goto_model
-              .get_goto_function(violation->source_location().get_function())
+            parse_option.goto_model.get_goto_function(to_fun_name)
               .body.instructions.begin();
-          it !=
-          parse_option.goto_model
-            .get_goto_function(violation->source_location().get_function())
-            .body.instructions.end();
+          it != parse_option.goto_model.get_goto_function(to_fun_name)
+                  .body.instructions.end();
           ++it)
       {
         if(it->location_number == violation->location_number)
