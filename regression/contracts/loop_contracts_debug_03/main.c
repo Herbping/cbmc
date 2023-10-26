abcdb013e25022e1f72c263ce06833569995f152
Qinheping Hu <qinhh@amazon.com>
2023-10-26 00:02:56 -0500

    Try to reproduce bugs without synthesis

--- regression/contracts/loop_contracts_debug_03/main.c
+++ regression/contracts/loop_contracts_debug_03/main.c
@@ -2,13 +2,14 @@
 
 void main()
 {
-  unsigned long len;
+  unsigned short len;
   __CPROVER_assume(len >= 8);
   char *array = malloc(len);
   const char *end = array + len;
   unsigned s = 0;
 
   while(array != end)
+  __CPROVER_loop_invariant(__CPROVER_same_object(array, end) && __CPROVER_POINTER_OFFSET(array) <= __CPROVER_OBJECT_SIZE(array))
   {
     s += *array++;
   }
